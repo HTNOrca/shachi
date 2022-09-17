3aa4a8971e60a5307d033d223539bb56ef4ee507
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-09-17 16:32:10 -0400

    toggle ui

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -10,30 +10,45 @@ impl Plugin for UIPlugin {
         app.insert_resource(UIState::default())
             .add_plugin(EguiPlugin)
             .add_system(render_ui)
-            .add_system(update_state);
+            .add_system(update_state)
+            .add_system(ui_controller);
     }
 }
 
 pub struct UIState {
+    show_panel: bool,
     orca_count: u32,
 }
 
 impl Default for UIState {
     fn default() -> Self {
         Self {
+            show_panel: true,
             orca_count: 0,
         }
     }
 }
 
 fn render_ui(mut ctx: ResMut<EguiContext>, ui_state: Res<UIState>) {
-    SidePanel::new(Side::Right, "root").resizable(true).show(ctx.ctx_mut(), |ui| {
-        ui.heading("Analyzer");
-        ui.separator();
-        ui.label(format!("simulated orcas: {}", ui_state.orca_count));
-    });
+    let panel = SidePanel::new(Side::Right, "root").resizable(true);
+
+    if ui_state.show_panel {
+        panel.show(ctx.ctx_mut(), |ui| {
+            ui.heading("Analyzer");
+            ui.separator();
+            ui.label(format!("simulated orcas: {}", ui_state.orca_count));
+        });
+    }
 }
 
 fn update_state(query: Query<&Orca>, mut ui_state: ResMut<UIState>) {
     ui_state.orca_count = query.iter().len() as u32;
+}
+
+fn ui_controller(
+    keys: Res<Input<KeyCode>>, mut ui_state: ResMut<UIState>
+) {
+    if keys.just_pressed(KeyCode::Escape) {
+        ui_state.show_panel = !ui_state.show_panel;
+    }
 }
