ea8c37f1f5ee798327e0a839566b3d8488c6ef94
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-09-17 18:24:06 -0400

    2d mod picking (#4)
    * 2d mod picking
    
    * camera follow

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -3,8 +3,9 @@ use bevy_egui::{
     egui::{containers::panel::Side, SidePanel, Window},
     EguiContext, EguiPlugin,
 };
+use bevy_mod_picking::events::PickingEvent;
 
-use crate::orca::Orca;
+use crate::{camera::CameraFollow, orca::Orca};
 
 pub struct UIPlugin;
 
@@ -14,9 +15,13 @@ impl Plugin for UIPlugin {
             .add_plugin(EguiPlugin)
             .add_system(render_ui)
             .add_system(update_state)
-            .add_system(ui_controller);
+            .add_system(ui_controller)
+            .add_system(select_controller)
+            .add_system(deselect_controller);
     }
 }
+
+pub struct SelectedOrca(Entity);
 
 pub struct UIState {
     show_panel: bool,
@@ -32,7 +37,12 @@ impl Default for UIState {
     }
 }
 
-fn render_ui(mut ctx: ResMut<EguiContext>, ui_state: Res<UIState>) {
+fn render_ui(
+    mut ctx: ResMut<EguiContext>,
+    ui_state: Res<UIState>,
+    selected: Option<Res<SelectedOrca>>,
+    query: Query<&Orca>,
+) {
     let panel = SidePanel::new(Side::Right, "root").resizable(true);
 
     if ui_state.show_panel {
@@ -40,6 +50,16 @@ fn render_ui(mut ctx: ResMut<EguiContext>, ui_state: Res<UIState>) {
             ui.heading("Analyzer");
             ui.separator();
             ui.label(format!("simulated orcas: {}", ui_state.orca_count));
+
+            if let Some(selected) = selected {
+                if let Ok(orca) = query.get(selected.0) {
+                    ui.separator();
+                    ui.label(format!("gender: {}", orca.gender.to_string()));
+                    ui.label(format!("age: {}", orca.age));
+                    ui.label(format!("mass: {}", orca.mass));
+                    ui.label(format!("type: {}", orca.orca_type.to_string()));
+                }
+            }
         });
     }
 }
@@ -49,7 +69,26 @@ fn update_state(query: Query<&Orca>, mut ui_state: ResMut<UIState>) {
 }
 
 fn ui_controller(keys: Res<Input<KeyCode>>, mut ui_state: ResMut<UIState>) {
-    if keys.just_pressed(KeyCode::Escape) {
+    if keys.just_pressed(KeyCode::Tab) {
         ui_state.show_panel = !ui_state.show_panel;
+    }
+}
+
+fn select_controller(mut cmd: Commands, mut events: EventReader<PickingEvent>) {
+    for evt in events.iter() {
+        match evt {
+            PickingEvent::Clicked(entity) => {
+                cmd.insert_resource(SelectedOrca(*entity));
+                cmd.insert_resource(CameraFollow(*entity));
+            },
+            _ => {},
+        }
+    }
+}
+
+fn deselect_controller(mut cmd: Commands, keys: Res<Input<KeyCode>>) {
+    if keys.just_pressed(KeyCode::Escape) {
+        cmd.remove_resource::<SelectedOrca>();
+        cmd.remove_resource::<CameraFollow>();
     }
 }
