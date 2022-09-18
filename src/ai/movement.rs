fed53d1e6dcbce3946504e51038cee8397a0cd77
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-09-18 03:16:27 -0400

    boid rewrite with rapier

--- src/ai/movement.rs
+++ src/ai/movement.rs
@@ -189,97 +189,12 @@ fn fish_sight(
 
 fn orca_boid_ai(
     time: Res<Time>,
-    mut query: Query<
-        (
-            Entity,
-            &mut Transform,
-            &OrcaNeighbouring,
-            &Movement,
-            &mut RigidBody,
-        ),
-        With<Orca>,
-    >,
-    target_query: Query<&Transform, Without<Orca>>,
+    mut query: Query<(Entity, &mut Transform, &OrcaNeighbouring, &Movement), With<Orca>>,
 ) {
-    let mut force_updates: HashMap<Entity, Vec2> = HashMap::new();
-    for (entity, trans, neighbouring, movement, rb) in query.iter() {
+    for (entity, trans, neighbouring, movement) in &query {
         let neighbours = &neighbouring.pod_members;
 
-        if neighbours.len() == 0 {
-            continue;
-        }
-
-        let mut cur_force = force_updates.get(&entity).unwrap_or(&Vec2::ZERO).clone();
-
-        // randomness force
-        use std::f32::consts::PI;
-
-        use rand::{thread_rng, Rng};
-
-        if rb.velocity.length() != 0. {
-            let rand: i32 = thread_rng().gen_range(0..(movement.wander_angle as i32));
-            let angle_deviation = ((rand - 180) as f32) * PI / 180.;
-            let forward = rb.velocity.angle_between(Vec2::X);
-            let random_force = Mat2::from_angle(angle_deviation + forward) * Vec2::X;
-            cur_force += random_force * movement.randomess;
-        }
-
-        // alignment (attempt to face same direction as neighbours)
-        let avg_heading = query
-            .iter_many(neighbours)
-            .fold(Vec2::ZERO, |acc, (_, _, _, _, rb)| acc + rb.velocity)
-            / neighbours.len() as f32;
-        cur_force += (avg_heading - rb.velocity.normalize()) * movement.alignment;
-
-        // cohesion
-        let avg_position = query
-            .iter_many(neighbours)
-            .fold(Vec3::ZERO, |acc, (_, trans, _, _, _)| {
-                acc + trans.translation
-            })
-            / neighbours.len() as f32;
-        cur_force += (avg_position - trans.translation).truncate() * movement.coherence;
-
-        // separation
-        let seperation_force =
-            query
-                .iter_many(neighbours)
-                .fold(Vec2::ZERO, |acc, (_, other_trans, _, _, _)| {
-                    let dist = trans.translation.distance(other_trans.translation);
-                    let dir = (trans.translation - other_trans.translation).truncate();
-                    // TODO careful for division by zero
-                    acc + dir / dist
-                });
-        cur_force += seperation_force * movement.seperation;
-
-        // avoidance
-        let avoidance_force =
-            50. / (100. - trans.translation.truncate().length()).clamp(0.00001, 1e10);
-        cur_force += -trans.translation.truncate().normalize() * avoidance_force;
-
-        // target
-        if let Some(target) = movement.target {
-            if let Ok(target_trans) = target_query.get(target) {
-                let target_force = (target_trans.translation - trans.translation).truncate();
-                cur_force += target_force * movement.tracking;
-            }
-        }
-
-        force_updates.insert(entity, cur_force);
-    }
-
-    // update all the forces
-    for (e, _, _, ai, mut rb) in query.iter_mut() {
-        if let Some(force) = force_updates.get(&e) {
-            rb.force +=
-                *force * 1000. * ai.speed_scale * time.delta().as_micros() as f32 / 1_000_000.0;
-        }
-    }
-
-    // rotate boid to face direction of travel
-    for (e, mut trans, _, _, rb) in query.iter_mut() {
-        let facing = rb.velocity.angle_between(Vec2::X);
-        trans.rotation = Quat::from_rotation_z(facing);
+        // alignment
     }
 }
 
